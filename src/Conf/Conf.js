584c6b78918179fe0f67a55c7fb9955d8e60c5a4
tejas-gojariya <tejasgojariya036@gmail.com>
2024-01-08 11:37:09 +0530

    conf file

---
+++ src/Conf/Conf.js
@@ -0,0 +1,12 @@
+const conf = {
+    appwriteUrl: String(
+        import.meta.env.VIRE_APPWRITE_URL),
+    appwriteProjectId: String(
+        import.meta.env.VIRE_PROJECT_ID),
+    appwriteDataBaseId: String(
+        import.meta.env.VIRE_DATABASE_ID),
+    appwriteCollectionId: String(
+        import.meta.env.VIRE_COLLECTION_ID),
+    appwriteBucketId: String(
+        import.meta.env.VIRE_BUCKET_ID),
+};
