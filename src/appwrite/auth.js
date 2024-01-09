f09b9a3aed3db8fb8ce357c0008142c29d24fcfc
tejas-gojariya <tejasgojariya036@gmail.com>
2024-01-10 00:20:29 +0530

    some changes and Update UI

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,25 +1,23 @@
-import conf from "../Conf.js";
+// import conf from '../conf/conf.js';
+import Conf from '../Conf/Conf';
 import { Client, Account, ID } from "appwrite";
 
+
 export class AuthService {
-    Client = new Client();
+    client = new Client();
     account;
 
     constructor() {
-        this.Client.setEndpoint(conf.appwriteUrl).setProject(
-            conf.appwriteProjectId
-        );
-        this.account = new Account(this.Client);
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+
     }
 
     async createAccount({ email, password, name }) {
         try {
-            const userAccount = await this.account.create(
-                ID.unique(),
-                email,
-                password,
-                name
-            );
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
             if (userAccount) {
                 // call another method
                 return this.login({ email, password });
@@ -38,23 +36,27 @@ export class AuthService {
             throw error;
         }
     }
+
     async getCurrentUser() {
         try {
             return await this.account.get();
         } catch (error) {
-            console.log("Appwrite servise :: getCurrentUser :: error", error);
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
         }
+
         return null;
     }
 
     async logout() {
+
         try {
-            await this.account.deleteSessions()
+            await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwtite service :: logout :: error", error)
+            console.log("Appwrite serive :: logout :: error", error);
         }
     }
 }
 
 const authService = new AuthService();
-export default authService;
+
+export default authService
